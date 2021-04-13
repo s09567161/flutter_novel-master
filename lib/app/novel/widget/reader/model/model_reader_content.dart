26b1f473c7d8b6b99e22f379df7c3866e28ba436
Administrator <http:/192.168.33/wwx/qianji_app.git>
2021-04-13 16:06:26 +0800

    自动化打包测试

--- lib/app/novel/widget/reader/model/model_reader_content.dart
+++ lib/app/novel/widget/reader/model/model_reader_content.dart
@@ -373,7 +373,7 @@ assert(paragraphContents!=null&&paragraphContents.length>0);
           }
         }
       } else {
-        print('-----纯文本---311212121-------------');
+        print('-----纯文本---3112121-------------');
         if(a==0&&index==0){
           ///绘制内标题
           viewModel.textPainter.text = TextSpan(
